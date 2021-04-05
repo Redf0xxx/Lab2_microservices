fe973902c6ad4be9cd3833837453f0aa25642eeb
Koklush <oliakol26@gmail.com>
2021-04-05 23:56:02 +0300

    Lab 4 UPD

---
+++ Lab 4/logging_service/app_3.py
@@ -0,0 +1,37 @@
+
+from flask import request, Flask
+import hazelcast
+app = Flask(__name__)
+
+messag={}
+
+
+client = hazelcast.HazelcastClient(
+        cluster_name="dev",
+        cluster_members=[
+        "192.168.99.100:5703",
+        "192.168.99.100:5704",
+        "192.168.99.100:5705",
+    ],
+    lifecycle_listeners=[lambda state: print("STATE:", state), ]
+)
+
+@app.route('/logging-service', methods=['GET', 'POST'])
+def log_service():
+    if request.method == 'POST':
+        return proc_post_request()
+    else:
+        return ','.join([msg for msg in messag.values()])
+distributed_map = client.get_map("map")
+
+def proc_post_request():
+    print(f'RECEIVED request: {request}')
+    msg = request.json["uuid"]
+    uuid = request.json["msg"]
+    distributed_map.put(uuid, msg)
+    #messag.update({request.json["uuid"]: request.json["msg"]})
+    print(f'SAVED to messages')
+    return app.response_class(status=200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5705, debug=True)
